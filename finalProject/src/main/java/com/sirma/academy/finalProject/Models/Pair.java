60688d9781646f1c8a9c6b4cc6b004c8d44ba920
Ventsislav Kisyov <ventsislav755@gmail.com>
2023-12-22 23:47:03 +0200

    edited model class Pair

--- finalProject/src/main/java/com/sirma/academy/finalProject/Models/Pair.java
+++ finalProject/src/main/java/com/sirma/academy/finalProject/Models/Pair.java
@@ -1,47 +1,13 @@
 package com.sirma.academy.finalProject.Models;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
 public class Pair {
     private int projectId;
     private int empId1;
     private int empId2;
     private long commonDays;
-
-    public Pair(int projectId, int empId1, int empId2, long commonDays) {
-        this.projectId = projectId;
-        this.empId1 = empId1;
-        this.empId2 = empId2;
-        this.commonDays = commonDays;
-    }
-
-    public int getProjectId() {
-        return projectId;
-    }
-
-    public void setProjectId(int projectId) {
-        this.projectId = projectId;
-    }
-
-    public int getEmpId1() {
-        return empId1;
-    }
-
-    public void setEmpId1(int empId1) {
-        this.empId1 = empId1;
-    }
-
-    public int getEmpId2() {
-        return empId2;
-    }
-
-    public void setEmpId2(int empId2) {
-        this.empId2 = empId2;
-    }
-
-    public long getCommonDays() {
-        return commonDays;
-    }
-
-    public void setCommonDays(long commonDays) {
-        this.commonDays = commonDays;
-    }
 }
