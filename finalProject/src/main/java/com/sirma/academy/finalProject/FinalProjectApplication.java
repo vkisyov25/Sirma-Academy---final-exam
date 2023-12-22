e0a78fc78e41c104af291e6ba4d6ec278a436fa2
Ventsislav Kisyov <ventsislav755@gmail.com>
2023-12-23 00:47:43 +0200

    fixed problem due to missing database

--- finalProject/src/main/java/com/sirma/academy/finalProject/FinalProjectApplication.java
+++ finalProject/src/main/java/com/sirma/academy/finalProject/FinalProjectApplication.java
@@ -2,11 +2,14 @@ package com.sirma.academy.finalProject;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
 public class FinalProjectApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(FinalProjectApplication.class, args);
 	}
 
