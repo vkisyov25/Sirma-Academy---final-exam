f519cd19df4cd5d3f8ce6f0d29d7a06c6a148726
Ventsislav Kisyov <ventsislav755@gmail.com>
2023-12-22 23:45:55 +0200

    edited model class Employee

--- finalProject/src/main/java/com/sirma/academy/finalProject/Models/Employee.java
+++ finalProject/src/main/java/com/sirma/academy/finalProject/Models/Employee.java
@@ -1,49 +1,17 @@
 package com.sirma.academy.finalProject.Models;
 
+import jakarta.persistence.Entity;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
 import java.time.LocalDate;
 
+@Getter
+@AllArgsConstructor
 public class Employee {
     private int empID;
     private int projectID;
     private LocalDate dateFrom;
     private LocalDate dateTo;
 
-    public Employee(int empID, int projectID, LocalDate dateFrom, LocalDate dateTo) {
-        this.empID = empID;
-        this.projectID = projectID;
-        this.dateFrom = dateFrom;
-        this.dateTo = dateTo;
-    }
-
-    public int getEmpID() {
-        return empID;
-    }
-
-    public void setEmpID(int empID) {
-        this.empID = empID;
-    }
-
-    public int getProjectID() {
-        return projectID;
-    }
-
-    public void setProjectID(int projectID) {
-        this.projectID = projectID;
-    }
-
-    public LocalDate getDateFrom() {
-        return dateFrom;
-    }
-
-    public void setDateFrom(LocalDate dateFrom) {
-        this.dateFrom = dateFrom;
-    }
-
-    public LocalDate getDateTo() {
-        return dateTo;
-    }
-
-    public void setDateTo(LocalDate dateTo) {
-        this.dateTo = dateTo;
-    }
 }
