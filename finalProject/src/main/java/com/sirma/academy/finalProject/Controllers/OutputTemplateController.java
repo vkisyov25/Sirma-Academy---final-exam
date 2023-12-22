98f26f93e93eea8d39102890ae6d75bbb9d3037e
Ventsislav Kisyov <ventsislav755@gmail.com>
2023-12-23 00:38:25 +0200

    created OutputTemplateController

---
+++ finalProject/src/main/java/com/sirma/academy/finalProject/Controllers/OutputTemplateController.java
@@ -0,0 +1,29 @@
+package com.sirma.academy.finalProject.Controllers;
+
+import com.sirma.academy.finalProject.Models.OutputTemplate;
+import com.sirma.academy.finalProject.Services.OutputTemplateService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/view")
+public class OutputTemplateController {
+    private OutputTemplateService outputTemplateService;
+
+    @Autowired
+    public OutputTemplateController(OutputTemplateService outputTemplateService) {
+        this.outputTemplateService = outputTemplateService;
+    }
+
+    @GetMapping("/allPairs")
+    public List<OutputTemplate> allPairs() {
+        return outputTemplateService.commonDaysOfEachPair();
+    }
+
+    @GetMapping("/maxCommonDays")
+    public OutputTemplate pairWithTheMostCommonDays() {
+        return outputTemplateService.maxCommonDays();
+    }
+}
