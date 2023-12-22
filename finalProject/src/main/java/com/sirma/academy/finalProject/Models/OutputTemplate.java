e86fdf34e74d936f5de11f5329baa2b04810d6e4
Ventsislav Kisyov <ventsislav755@gmail.com>
2023-12-22 23:48:02 +0200

    edited model class OutputTemplate

--- finalProject/src/main/java/com/sirma/academy/finalProject/Models/OutputTemplate.java
+++ finalProject/src/main/java/com/sirma/academy/finalProject/Models/OutputTemplate.java
@@ -1,7 +1,10 @@
 package com.sirma.academy.finalProject.Models;
 
+import lombok.Getter;
+
 import java.util.Map;
 
+@Getter
 public class OutputTemplate {
     private  int empId1;
     private  int empId2;
@@ -12,38 +15,6 @@ public class OutputTemplate {
         this.empId1 = empId1;
         this.empId2 = empId2;
         this.commonDays = commonDays;
-        this.projectDaysMap = projectDaysMap;
-    }
-
-    public int getEmpId1() {
-        return empId1;
-    }
-
-    public void setEmpId1(int empId1) {
-        this.empId1 = empId1;
-    }
-
-    public int getEmpId2() {
-        return empId2;
-    }
-
-    public void setEmpId2(int empId2) {
-        this.empId2 = empId2;
-    }
-
-    public long getCommonDays() {
-        return commonDays;
-    }
-
-    public void setCommonDays(long commonDays) {
-        this.commonDays = commonDays;
-    }
-
-    public Map<Integer, Long> getProjectDaysMap() {
-        return projectDaysMap;
-    }
-
-    public void setProjectDaysMap(Map<Integer, Long> projectDaysMap) {
         this.projectDaysMap = projectDaysMap;
     }
 }
