7a656949f471f0bf069b1fa7661f07691cd7026f
Ventsislav Kisyov <ventsislav755@gmail.com>
2023-12-23 23:48:17 +0200

    eddit anotation of FinalProjectAplication class, because there is a mistake when a created db for employees

--- finalProject/src/main/java/com/sirma/academy/finalProject/FinalProjectApplication.java
+++ finalProject/src/main/java/com/sirma/academy/finalProject/FinalProjectApplication.java
@@ -3,9 +3,8 @@ package com.sirma.academy.finalProject;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
-
-
-@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+@SpringBootApplication(scanBasePackages = "com.sirma.academy.finalProject")
 public class FinalProjectApplication {
 
     public static void main(String[] args) {
