da2c92b420abac94a38cd0423c9214d2dbfd8fe0
Ventsislav Kisyov <ventsislav755@gmail.com>
2023-12-23 19:03:22 +0200

    edited logic in this class(I merged the two services into one)

--- finalProject/src/main/java/com/sirma/academy/finalProject/Services/OutputTemplateService.java
+++ finalProject/src/main/java/com/sirma/academy/finalProject/Services/OutputTemplateService.java
@@ -6,6 +6,8 @@ import com.sirma.academy.finalProject.Models.Pair;
 import com.sirma.academy.finalProject.Utils.CSVReader;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDate;
+import java.time.temporal.ChronoUnit;
 import java.util.HashMap;
 import java.util.LinkedList;
 import java.util.List;
@@ -13,8 +15,56 @@ import java.util.Map;
 
 @Service
 public class OutputTemplateService {
-    public  List<OutputTemplate> commonDaysOfEachPair() {
-        List<Pair> pairs = PairService.findPairs(CSVReader.readCSV("D:\\JavaPrograms\\Sirma-Academy---final-exam\\finalProject\\src\\main\\resources\\input.cvs"));
+    public static List<Pair> findPairs(List<Employee> employees) {
+        List<Pair> pairs = new LinkedList<>();
+        for (int i = 0; i < employees.size(); i++) {
+            for (int j = i + 1; j < employees.size(); j++) {
+                if (employees.get(i).getEmpID() != employees.get(j).getEmpID()) {
+                    if (employees.get(i).getProjectID() == employees.get(j).getProjectID()) {
+                        int projectId = employees.get(i).getProjectID();
+                        int empId1 = employees.get(i).getEmpID();
+                        int empId2 = employees.get(j).getEmpID();
+                        long commonDays = calculateCommonDays(i, j, employees);
+                        if(commonDays!=0){
+                            Pair pair = new Pair(projectId, empId1, empId2, commonDays);
+                            pairs.add(pair);
+                        }
+                    }
+
+                }
+
+            }
+
+        }
+        return pairs;
+    }
+
+    public static long calculateCommonDays(int i, int j, List<Employee> employees) {
+
+        LocalDate startDate;
+        LocalDate endDate;
+        long commonDays = 0;
+
+        if (employees.get(i).getDateFrom().isBefore(employees.get(j).getDateFrom())) {
+            startDate = employees.get(j).getDateFrom();
+        } else {
+            startDate = employees.get(i).getDateFrom();
+        }
+
+        if (employees.get(i).getDateTo().isBefore(employees.get(j).getDateTo())) {
+            endDate = employees.get(i).getDateTo();
+        } else {
+            endDate = employees.get(j).getDateTo();
+        }
+
+        if (startDate.isBefore(endDate)) {
+            commonDays = ChronoUnit.DAYS.between(startDate, endDate);
+        }
+
+        return commonDays;
+    }
+    public  List<OutputTemplate> commonDaysOfEachPairForEachProject() {
+        List<Pair> pairs = findPairs(CSVReader.readCSV("D:\\JavaPrograms\\Sirma-Academy---final-exam\\finalProject\\src\\main\\resources\\input.cvs"));
         List<OutputTemplate> outputTemplateList = new LinkedList<>();
         Map<Integer, Long> map = new HashMap<>();
         boolean isSave = false;
@@ -40,8 +90,8 @@ public class OutputTemplateService {
             if (!isSave) {
                 int empId1 = pairs.get(i).getEmpId1();
                 int empId2 = pairs.get(i).getEmpId2();
-                map.put(pairs.get(i).getProjectId(), pairs.get(i).getCommonDays());
                 long commonDays = pairs.get(i).getCommonDays();
+                map.put(pairs.get(i).getProjectId(), commonDays);
                 OutputTemplate outputTemplate = new OutputTemplate(empId1, empId2, commonDays, map);
                 outputTemplateList.add(outputTemplate);
                 pairs.remove(i);
@@ -50,6 +100,7 @@ public class OutputTemplateService {
                 i--;
                 isSave = false;
             }
+            map = new HashMap<>();//to reset creating a new HashMap
 
             if (pairs.size() == 1) {
                 int empId1 = pairs.get(0).getEmpId1();
@@ -60,14 +111,13 @@ public class OutputTemplateService {
                 pairs.remove(i);
             }
 
-            map = new HashMap<>();
         }
         return outputTemplateList;
     }
 
     public OutputTemplate maxCommonDays(){
         OutputTemplate outputTemplate;
-        List<OutputTemplate> outputTemplateList = commonDaysOfEachPair();
+        List<OutputTemplate> outputTemplateList = commonDaysOfEachPairForEachProject();
         long maxCommonDays = 0;
         int maxCommonDaysIndex = 0;
         for (int i = 0; i < outputTemplateList.size(); i++) {
