c866ba5c8aab97ddd633d9a117035cf0d975fedf
Ventsislav Kisyov <ventsislav755@gmail.com>
2023-12-23 23:44:32 +0200

    added anotation Entity of the Employee model

--- finalProject/src/main/java/com/sirma/academy/finalProject/Models/Employee.java
+++ finalProject/src/main/java/com/sirma/academy/finalProject/Models/Employee.java
@@ -1,17 +1,41 @@
 package com.sirma.academy.finalProject.Models;
 
-import jakarta.persistence.Entity;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NonNull;
+import lombok.Setter;
+import org.hibernate.id.factory.internal.AutoGenerationTypeStrategy;
 
 import java.time.LocalDate;
 
 @Getter
-@AllArgsConstructor
+@Setter
+@Entity
+@Table(name="employees")
 public class Employee {
+    @Id
+    @GeneratedValue(strategy= GenerationType.AUTO)
+    private long ID;
+    @NonNull
     private int empID;
+    @NonNull
     private int projectID;
+    @NonNull
     private LocalDate dateFrom;
+    @NonNull
     private LocalDate dateTo;
+
+    public Employee(int empID, int projectID, LocalDate dateFrom, LocalDate dateTo) {
+        this.empID = empID;
+        this.projectID = projectID;
+        this.dateFrom = dateFrom;
+        this.dateTo = dateTo;
+    }
+
+
+    public Employee() {
+
+    }
 
 }
